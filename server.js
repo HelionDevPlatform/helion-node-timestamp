ee88b9e52d4bc72bdca3fac80566d98f7fde1be3
Neil O'Toole <neilotoole@apache.org>
2015-07-05 12:16:03 -0600

    fixes to server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var http = require('http'),
-  fs = require('fs');
+  fs = require('fs'),
+  port = process.env.PORT || 8888;
 
 http.createServer(function (req, res) {
   var contents = fs.readFileSync('timestamp.txt');
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end(contents);
-}).listen(8001, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:8001/');
+}).listen(parseInt(port, 10));
+
+console.log("Server running at\n  => http://0.0.0.0:" + port + "/\nCTRL + C to shutdown");
