275eec9f636875ddbe5a8e284e8f19684346deca
Neil O'Toole <neilotoole@apache.org>
2015-07-05 14:36:37 -0600

    cleanup

--- updateTimestamp.js
+++ updateTimestamp.js
@@ -2,16 +2,8 @@ var fs = require('fs'),
   moment = require('moment'),
   timestamp,
   username;
-// toISOString uses this format: '2012-11-04T14:51:06.157Z'
-// But we want this format: '2012-11-04_14.55.45'
-//timestamp = new Date().toISOString().
-//  replace(/T/, '_').      // replace T with a space
-//  replace(/\..+/, '').
-//  replace(/:/, '.');
 
 timestamp = moment().format('YYYY-MM-DD_HH.mm.ss');
-
-
 username = process.env.USER;
 
 // What we really want is: '2012-11-04_14.55.45_neilotoole'
@@ -22,7 +14,7 @@ fs.writeFile('timestamp.txt', timestamp, function(err) {
     return console.error(err);
   }
 
-  console.log('timestamp.txt was updated: ' + timestamp);
+  console.log(timestamp);
 });
 
 
