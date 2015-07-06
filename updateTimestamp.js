c1716cae645d245bd0a9d46108562319b0ab9fa9
Neil O'Toole <neilotoole@apache.org>
2015-07-06 00:47:58 -0600

    changes to timestamp format

--- updateTimestamp.js
+++ updateTimestamp.js
@@ -3,11 +3,11 @@ var fs = require('fs'),
   timestamp,
   username;
 
-timestamp = moment().format('YYYY-MM-DD_HH.mm.ss');
-username = process.env.USER;
 
-// What we really want is: '2012-11-04_14.55.45_neilotoole'
-timestamp = timestamp + '_' + username;
+timestamp = moment().format('YYYYMMDD-HHmmss');
+username = process.env.USER;
+// The format we want is: 20150706-004444-neilotoole
+timestamp = timestamp + '-' + username;
 
 fs.writeFile('timestamp.txt', timestamp, function(err) {
   if(err) {
--- update_timestamp.py
+++ update_timestamp.py
@@ -3,12 +3,11 @@
 import datetime
 import os
 
-
 username = os.environ["USER"]
 timestamp = datetime.datetime.now()
-datetime_format = '%Y-%m-%d_%H.%M.%S'
+datetime_format = '%Y%m%d-%H%M%S'
 formatted_time = timestamp.strftime(datetime_format)
-stamp = formatted_time + '_' + username
+stamp = formatted_time + '-' + username
 print stamp
 
 f = open('timestamp.txt', 'w')
